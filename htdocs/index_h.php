19e717f71e44bd8cb0e8d1688c82472a28f77a9a
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2016-02-29 11:24:49 +0300

    removed unreferenced files

--- htdocs/index_h.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>О компании "Строй-Маркетинг"</title>
-<meta http-equiv="Content-Language" content="ru">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
-<meta name="description" content="компания Строй-Маркетинг,  Санкт-Петербург, маркетинговые исследования, строительство, торговля техникой, обработка природного камня">
-<meta name="keywords" content="оценка недвижимости и бизнеса, разработка бизнес-планов, обследование технического состояния зданий и сооружений, природный камень, камнеобработка, бульдозер, экскаватор, прицеп"><link href="sm.css" rel="STYLESHEET" type="text/css">
-</head>
